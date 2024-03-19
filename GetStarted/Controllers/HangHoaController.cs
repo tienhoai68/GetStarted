830803f83f24b30dfe3ac436f1a535c56ee99b95
Tien Hoai <nguyentien1668@gmail.com>
2024-03-19 21:10:21 +0700

    Add project files.

---
+++ GetStarted/Controllers/HangHoaController.cs
@@ -0,0 +1,33 @@
+﻿using GetStarted.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetStarted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult getAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hanghoa);
+            return Ok();
+                    
+        }
+
+    }
+}
